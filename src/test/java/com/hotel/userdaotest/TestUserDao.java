6c23c78c2816eaf151b28981aae272a270806b20
47 <136698493@qq.com>
2017-04-30 21:25:44 +0800

    MD5

--- src/test/java/com/hotel/userdaotest/TestUserDao.java
+++ src/test/java/com/hotel/userdaotest/TestUserDao.java
@@ -1,20 +1,26 @@
 package com.hotel.userdaotest;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.commons.codec.binary.Hex;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.hotel.base.BaseJUnit;
 import com.hotel.dao.UserDAO;
-import com.hotel.model.User;
 
 public class TestUserDao extends BaseJUnit{
 	@Autowired
 	private UserDAO iu;
 	@Test
-	public void test(){
-		User u = new User();
-		u.setName("1234");
-		u.setPassword("1234");
-		iu.checkUser(u);
+	public void test() throws NoSuchAlgorithmException{
+//		User u = new User();
+//		u.setName("1234");
+//		u.setPassword("1234");
+//		iu.checkUser(u);
+		MessageDigest mdInst = MessageDigest.getInstance("MD5");
+
+		System.out.println(Hex.encodeHex(mdInst.digest("1234".getBytes())));
 	}
 }
